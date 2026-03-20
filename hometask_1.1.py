b0607ecdae1d2608667921d3d1904162941cbcf9
Lesya Sakhnovska <lesya.sakhnovska@gmail.com>
2026-03-20 21:53:45 +0000

    Updated Hometask 1_1

--- hometask_1.1.py
+++ hometask_1.1.py
@@ -1,21 +1,18 @@
-#Імпортуйте модуль datetime.
 from datetime import datetime
-
 
 #Створіть функцію get_days_from_today(date), яка розраховує кількість днів між заданою датою і поточною датою.
 def get_days_from_today(date):
+    try:
 #Перетворіть рядок дати у форматі 'РРРР-ММ-ДД' у об'єкт datetime.
-    datetime_object = datetime.strptime(date,"%Y-%m-%d")
+        datetime_object = datetime.strptime(date,"%Y-%m-%d")
 #Отримайте поточну дату, використовуючи datetime.today().
-    current_datetime = datetime.today()
+        current_datetime = datetime.today()
 #Розрахуйте різницю між поточною датою та заданою датою.
-    difference_in_time = current_datetime.toordinal() - datetime_object.toordinal()
+        difference_in_time = current_datetime.toordinal() - datetime_object.toordinal()
 #Поверніть різницю у днях як ціле число.
-    return difference_in_time
+        return difference_in_time
+    except ValueError:
+        return "Invalid date format. Please use YYYY-MM-DD."
+
 print(get_days_from_today("2024-12-05"))
-
-#Вимоги до завдання:
-
-#Функція приймає один параметр: date — рядок, що представляє дату у форматі 'РРРР-ММ-ДД' (наприклад, '2020-10-09').
-#Функція повертає ціле число, яке вказує на кількість днів від заданої дати до поточної. Якщо задана дата пізніша за поточну, результат має бути від'ємним.
 
